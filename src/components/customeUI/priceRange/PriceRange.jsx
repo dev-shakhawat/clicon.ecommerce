a8f9413f8fba39911b3f332e10a81a39417683c1
shakhawat.dev <shakhawat.code@gmail.com>
2025-07-08 09:09:36 +0600

    price range updated

--- src/components/customeUI/priceRange/PriceRange.jsx
+++ src/components/customeUI/priceRange/PriceRange.jsx
@@ -11,7 +11,6 @@ export default function PriceRange() {
   const [maxval , setMaxval] = useState(20000);
 
 
-
   const handleMinChange = (val , type) => {
     if(type === 'min'){
       const updatedval = Math.min(val , maxval);
@@ -29,17 +28,17 @@ export default function PriceRange() {
 
   return (
     <div>
-        
-        {/* price range */}
-        <div className="relative">
-            <input onChange={(e)=> handleMinChange(e.target.value , 'min')} value={minval} min={0} max={10000} step={40} type="range" className=" w-full h-2 rounded-full outline-none cursor-pointer absolute top-0 left-0 z-[10]   " />
-            <input onChange={(e)=> handleMinChange(e.target.value , 'max')} value={maxval} min={0} max={20000} step={40} type="range" className=" w-full h-2 rounded-full outline-none cursor-pointer absolute top-0 left-0 z-[10]   " />
+          
+        <div className={`relative h-[2px] after:content-[]  after:w-full after:h-[2px] after:bg-[#e4e7e9] after:absolute after:top-0 mt-2  `}>
+            <span style={{left: `${(minval / 20000) * 100}%` , width: `${((maxval - minval) / 20000) * 100}%`}} className='absolute top-0 z-1 left-0  h-[2px] bg-[#FA8232]  ' ></span>
+             <input step={50} value={minval} onChange={(e)=> handleMinChange(e.target.value , "min")} type="range" min={0} max={20000} className='absolute -top-[8px] z-1 left-0 w-full appearance-none slider pointer-events-none  ' />
+            <input step={50} value={maxval} onChange={(e)=> handleMinChange(e.target.value , "max")} type="range" min={0} max={20000} className='absolute -top-[8px] z-1 left-0 w-full appearance-none slider pointer-events-none  ' />
         </div>
 
         {/* price value */}
         <div className="flex justify-between pt-5">
-            <span className='text-sm font-Public_Sans text-[#475156]'>Min: {minval}</span>
-            <span className='text-sm font-Public_Sans text-[#475156]'>Max: {maxval}</span>
+            <span className='text-sm font-Public_Sans text-[#475156] border border-gray-200 py-2 px-3  '>Min: {minval}</span>
+            <span className='text-sm font-Public_Sans text-[#475156] border border-gray-200 py-2 px-3  '>Max: {maxval}</span>
         </div>
 
 
